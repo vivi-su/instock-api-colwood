86722819772ffbc9de9cfdcae4a03f6b494b2ed3
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2022-11-22 20:50:14 -0800

    S22BCC-16-API to POST and CREATE a New Warehouse

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,4 +1,5 @@
 const knex = require("knex")(require("../knexfile"));
+const { v4: uuidv4 } = require("uuid");
 
 exports.index = (_req, res) => {
   knex("warehouses")
@@ -30,7 +31,6 @@ exports.singleWarehouse = (req, res) => {
     );
 };
 
-
 exports.warehouseInventories = (req, res) => {
   knex("inventories")
     .where({ warehouse_id: req.params.id })
@@ -44,4 +44,36 @@ exports.warehouseInventories = (req, res) => {
           `Error retrieving inventories for Warehouse ${req.params.id} ${err}`
         )
     );
+};
+
+exports.addWarehouse = (req, res) => {
+  // Validate the request body for required data
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_position ||
+    !req.body.contact_phone ||
+    !req.body.contact_email
+  ) {
+    return res
+      .status(400)
+      .send(
+        "Please make sure to provide warehouse_name, address, city, country, contact_name, contact_position, contact_phone and contact_email fields in a request"
+      );
+  }
+
+  const newWarehouseId = uuidv4();
+  knex("warehouses")
+    .insert({ ...req.body, id: newWarehouseId })
+    .then((_data) => {
+      knex("warehouses")
+        .where({ id: newWarehouseId })
+        .then((data) => {
+          res.status(201).json(data[0]);
+        });
+    })
+    .catch((err) => res.status(400).send(`Error creating Warehouse: ${err}`));
 };
